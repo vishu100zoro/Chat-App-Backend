6e6051e2ae50953d80e18d57993ccb83677f4728
vishu100zoro <vishu100zoro@gmail.com>
2024-02-17 23:46:39 +0530

    sixth commit

--- server.js
+++ server.js
@@ -8,14 +8,7 @@ const cors= require('cors');
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(cors(
-    {
-        origin:[process.env.FRONTEND_URL],
-        method:["GET","POST","PUT","DELETE"],
-        // allowedHeaders: ['Content-Type', 'Authorization'],
-        credentials:true,
-    }
-));
+app.use(cors());
 
 app.use('/users', userRoutes);
 const {connectDB} =require('./connection.js');
@@ -28,7 +21,15 @@ connectDB();
 
 const server=require('http').createServer(app);
 const PORT=5001;
-const io=require('socket.io')(server)
+const io = require('socket.io')(server, {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      method: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true,
+    }
+  });
+  
 
 
 
